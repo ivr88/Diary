b358dd70785c3db1bb616c920bf4746ecfc49c36
ivr88 <ivr88@yandex.ru>
2024-12-14 20:30:25 +0300

    Update ViewController.swift
    Выставление фонового цвета для проверки отображения

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Diary
-//
-//  Created by Вадим Исламов on 14.12.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
